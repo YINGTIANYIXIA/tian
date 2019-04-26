5b97f9513187ea4e8b499801f5f94e5ee29a8734
YINGTIANYIXIA <yingtianyixia@163.com>
2019-04-26 08:47:25 +0800

    运算

---
+++ YUN.php
@@ -0,0 +1,26 @@
+<?php
+print_r(array(['1','2','3','4','5','6','7','8',]9));
+function static yun($num1,$num2){
+	$a= [];
+	$len=count($num1);
+	if ($len==0) {
+		echo $arr;
+	}
+$arr= count();
+for ($i=0; $i < count($len); $i++) { 
+	for ($j=0; $j < count($len); $j++) { 
+		if ($a[i]<$len) {
+			$num++;
+		}
+	}
+}
+echo "$num1";
+}
+
+
+
+
+
+
+
+?>
