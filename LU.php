bbd8f70ed2dc4d447085e9b5786ad5c843241478
YINGTIANYIXIA <yingtianyixia@163.com>
2019-05-06 08:52:42 +0800

    路线

---
+++ LU.php
@@ -0,0 +1,25 @@
+<?php
+
+
+/**
+* 
+*/
+class LU
+{
+	
+	public static function CountSteps($x,$y){
+		
+
+		$sum = $x+$y;
+
+		 echo $sum;
+	}
+
+}
+
+$r = LU::CountStep(1,1);
+var_dump($r);
+
+
+
+?>
