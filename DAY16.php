0e73b318a4a3681f4211f84ebd960325e3aa4f95
YINGTIANYIXIA <yingtianyixia@163.com>
2019-05-28 08:49:03 +0800

    shu

---
+++ DAY16.php
@@ -0,0 +1,29 @@
+<?php
+
+
+/**
+* 
+*/
+class Suan
+{
+	$target = 7;
+	$array=[
+			[1,2,8,9],
+			[2,4,9,12],
+			[4,7,10,13],
+			[6,8,11,15]
+		];
+	public static function Find($target, $array)
+	{
+		$str = [];
+		foreach ($array as $k => $v) {
+			$target = $array[];
+			$str[]=$target;
+
+		}
+		return true;
+	}
+
+}
+
+
