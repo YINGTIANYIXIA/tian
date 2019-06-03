5383ee9d9938515eb51c6bf4517f7a09a7e13269
YINGTIANYIXIA <yingtianyixia@163.com>
2019-06-03 08:48:55 +0800

    -m路线

---
+++ DAY20.php
@@ -0,0 +1,24 @@
+<?php
+/**
+* 
+*/
+class Suan//定义一个函数
+{
+	
+	public static function CountSteps($x,$y)//定义一个方法
+	{
+		$sum = 0;
+		// foreach ($sum as $k => $v) {
+		// 	if ($x==0&&$y==1) {
+		// 		$x+1 = $y;
+		// 		$y = $sum;
+
+		// 	}
+		// }
+		return $sum;
+	}
+}
+
+$P = Suan::CountSteps(1,1);
+echo 2;
+?>
